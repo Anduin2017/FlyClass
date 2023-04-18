6cf201f4828b610ab691bef6d5c17aef689ea282
Anduin Xue <anduin@aiursoft.com>
2023-04-18 10:29:47 +0000

    Better experience.

--- src/Models/ManageViewModels/ChangePasswordViewModel.cs
+++ src/Models/ManageViewModels/ChangePasswordViewModel.cs
@@ -6,17 +6,17 @@ public class ChangePasswordViewModel
 {
     [Required]
     [DataType(DataType.Password)]
-    [Display(Name = "Current password")]
+    [Display(Name = "当前密码")]
     public string OldPassword { get; set; }
 
     [Required]
     [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
     [DataType(DataType.Password)]
-    [Display(Name = "New password")]
+    [Display(Name = "新密码")]
     public string NewPassword { get; set; }
 
     [DataType(DataType.Password)]
-    [Display(Name = "Confirm new password")]
+    [Display(Name = "确认新密码")]
     [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
     public string ConfirmPassword { get; set; }
 }
--- src/wwwroot/js/site.js
+++ src/wwwroot/js/site.js
@@ -2,3 +2,29 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var initDarkTheme = function () {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        // dark mode
+        $('.navbar-light').addClass('navbar-dark');
+        $('.navbar-light').removeClass('navbar-light');
+        $('body').addClass('bg-dark');
+        $('body').css('color', 'white');
+        $('.modal-content').addClass('bg-dark');
+        $('.modal-content').css('color', 'white');
+        $('.container-fluid').addClass('bg-dark');
+        $('.container-fluid').css('color', 'white');
+        $('.list-group-item').addClass('bg-dark');
+        $('.list-group-item').css('color', 'white');
+        $('.content-wrapper').addClass('bg-dark');
+        $('.card').addClass('bg-dark');
+        $('.bg-light').addClass('bg-dark');
+        $('.bg-light').removeClass('bg-light');
+        $('.bg-white').addClass('bg-dark');
+        $('.bg-white').removeClass('bg-white');
+        $('.bd-footer').addClass('bg-dark');
+        $('table').addClass('table-dark');
+    }
+}
+initDarkTheme();
+
+window.matchMedia('(prefers-color-scheme: dark)').addListener(initDarkTheme);
