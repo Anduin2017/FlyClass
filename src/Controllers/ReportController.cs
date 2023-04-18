66c958b36f19bd9e950189e095b46f33b69b1624
Anduin Xue <anduin@aiursoft.com>
2023-04-18 09:59:51 +0000

    Reporting feature.

---
+++ src/Controllers/ReportController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using FlyClass.Data;
+using FlyClass.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using FlyClass.Models.SubmitViewModels;
+using System.Security.Claims;
+using FlyClass.Models.ReportViewModels;
+
+namespace FlyClass.Controllers;
+
+[Authorize(Roles = "Admin")]
+public class ReportController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public ReportController(ApplicationDbContext dbContext)
+    {
+        this._context = dbContext;
+    }
+
+    public async Task<IActionResult> Index([FromQuery]DateTime? start = null, [FromQuery]DateTime? end = null)
+    {
+        if (start == null)
+        {
+            start = DateTime.MinValue;
+        }
+        if (end == null)
+        {
+            end = DateTime.MaxValue;
+        }
+
+        var allEvents = await _context.TeachEvents
+            .Include(t => t.Teacher)
+            .Include(t => t.Site)
+            .Where(t => t.EventTime > start)
+            .Where(t => t.EventTime < end)
+            .ToListAsync();
+
+        return View(new ReportViewModel 
+        {
+            PaidByPerson = allEvents.GroupBy(t => t.TeacherId).Where(t => t.Any()).ToList(),
+            PaidBySite = allEvents.GroupBy(t => t.SiteId).Where(t => t.Any()).ToList(),
+        });
+    }
+}
---
+++ src/Models/ReportViewModels/ReportViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlyClass.Models.ReportViewModels;
+
+public class ReportViewModel
+{
+    [Display(Name = "查询开始时间")]
+    public DateTime? Start { get; set; } = null;
+    [Display(Name = "查询结束时间")]
+    public DateTime? End { get; set; } = null;
+
+    public List<IGrouping<string, TeachEvent>> PaidByPerson { get; set; }
+    public List<IGrouping<int, TeachEvent>> PaidBySite { get; set; }
+}
